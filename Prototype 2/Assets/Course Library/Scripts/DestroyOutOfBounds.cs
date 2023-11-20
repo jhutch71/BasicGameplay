5bd0e7c3a7f46fbe977748257e617f943d01a7d0
Tara Mallick <taramallick09@gmail.com>
2023-11-20 13:57:58 -0500

    unity 2.4 done

--- Prototype 2/Assets/Course Library/Scripts/DestroyOutOfBounds.cs
+++ Prototype 2/Assets/Course Library/Scripts/DestroyOutOfBounds.cs
@@ -10,7 +10,7 @@ public class DestroyOutOfBounds : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -23,6 +23,7 @@ public class DestroyOutOfBounds : MonoBehaviour
         else if (transform.position.z < lowerBound)
         {
             Destroy(gameObject);
+            Debug.Log("Game Over!");
         }
     }
 }
